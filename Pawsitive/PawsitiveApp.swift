97d95a1a3d9933ba28bb44d847c1e4077fc5b6ff
Michael David Sin <146686816+michaeldavidsinn@users.noreply.github.com>
2026-08-21 16:31:35 +0700

    fix: initial commit

---
+++ Pawsitive/PawsitiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsitiveApp.swift
+//  Pawsitive
+//
+//  Created by Michael David Sin on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PawsitiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
